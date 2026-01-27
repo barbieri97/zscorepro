2cb51a0dad8e2280792a88c76c778b67b7200d12
barbieri97 <andrepb636@gmail.com>
2026-01-27 09:15:53 -0300

    add analytics da vercel

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
+
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
     <NuxtLayout>
+      <Analytics />
       <NuxtPage />
     </NuxtLayout>
   </UApp>
