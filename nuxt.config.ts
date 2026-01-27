7fcae35dc2eedb4a60d54e08e986045a22480bfc
barbieri97 <andrepb636@gmail.com>
2026-01-27 09:03:28 -0300

    add gtag e remove ntfy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxthub/core'
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "@nuxthub/core",
+    "nuxt-gtag",
   ],
 
+  gtag: {
+    id: "G-C6P9CVNT6V",
+  },
+
   colorMode: {
-    preference: 'dark',
-    fallback: 'dark',
+    preference: "dark",
+    fallback: "dark",
   },
   runtimeConfig: {
     public: {
-      appVersion: process.env.npm_package_version || '1.0.0'
-    }
-  }
-})
+      appVersion: process.env.npm_package_version || "1.0.0",
+    },
+  },
+});
